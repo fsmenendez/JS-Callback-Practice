3dcb5a0f19bbe1b768c32697ad69e48d952b5a1a
franc <fmenendezwork@gmail.com>
2023-05-04 17:52:22 -0700

    added arrow key movement to character

--- index.js
+++ index.js
@@ -7,23 +7,6 @@ let direction = null;
 let x = 100;
 let y = 250;
 
-/*function moveCharacter(){
-    if(direction === 'west'){
-        x = x - 1
-    }
-    if(direction === 'east'){
-        x = x + 1
-    }
-    if(direction === 'north'){
-        y = y + 1
-    }
-    if(direction === 'south'){
-        y = y - 1
-    }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-} */
-
 setInterval(function(){
     if(direction === 'west'){
         x = x - 1
@@ -52,3 +35,25 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
